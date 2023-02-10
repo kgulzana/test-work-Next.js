5a55fd0061ff7915fd51a04ac23cc3a2f64b9847
Gulzana Katkeldieva <gulzana.0185@mail.ru>
2023-02-10 11:33:28 -0600

    some change

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "355c257159acc093"; }
+/******/ 		__webpack_require__.h = function() { return "adb736141d6a5ac1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
